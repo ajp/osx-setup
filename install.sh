5a367791075958eda1f415e4800fabdf1e30c551
AJ Penninga <aj@penninga.net>
2015-07-13 09:52:31 -0400

    fixed quotes

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@
 #curl -sS https://getcomposer.org/installer | php
 
 # Install Homebrew
-ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)""
 brew doctor
 brew update
 
 # Install RVM
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-source .bash_profile
+source ~/.bash_profile
 gem install bundler
 
 ############
